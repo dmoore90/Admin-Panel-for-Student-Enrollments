40d244a0ca6f9dcc37e3e4da121de7ddf92e7b7e
r34p3r <danieljmoore123@gmail.com>
2021-06-15 16:37:59 -0700

    changed localhost to % in mysql.user for database auth error

--- api/db/connection.js
+++ api/db/connection.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize("carna_db", "user", "pass", {
 	// process.env.MYSQL_DATABASE, process.env.MYSQL_USERNAME, process.env.MYSQL_ROOT_PASSWORD
 	// host: process.env.MYSQL_HOST || 'localhost',
 	// port: process.env.MYSQL_LOCAL_PORT,
-	host: "127.0.0.1",
+	host: "carna_mysqldb_1",
 	port: "3306",
 	dialect: "mysql",
 	define: {

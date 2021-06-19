546998ab0b5ed9c0da8f26d81a07f1e6d5b48788
r34p3r <danieljmoore123@gmail.com>
2021-06-19 00:40:28 -0700

    added styles.css centered homepage

--- client/src/App/pages/Home.js
+++ client/src/App/pages/Home.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import '../../static/styles.css'
 
 class Home extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Home</h1>
+      <div class="top-wrapper">
         <ul>
-        <Link to={'./adminLogin'}>
-          <li><button variant="raised">Admin Login</button></li>
-        </Link>
-        <Link to={'./userLogin'}>
-          <li><button variant="raised">User Login</button></li>
-        </Link>              
+          <h1 class="title">Home</h1>
         </ul>
+        <div class="list-container">
+          <ul>
+            <li><Link to={'./adminLogin'}><button>Admin Login</button></Link></li>
+            <li><Link to={'./userLogin'}><button>User Login</button></Link></li>              
+          </ul>
+        </div>
       </div>      
     );
   }
